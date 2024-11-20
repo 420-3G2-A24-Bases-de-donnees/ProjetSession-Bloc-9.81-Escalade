6f4a08b50e0e850496187182811b0ea79915a16b
benjaminLemoyne <157721721+benjaminLemoyne@users.noreply.github.com>
2024-11-20 12:08:38 -0500

    Deuxième pouch

--- Creation_des_tables.sql
+++ Creation_des_tables.sql
@@ -1,72 +1,73 @@
 USE [Bloc_9.81]
 GO
 
---Enlever la table "Clients" si elle existe
 DROP TABLE IF EXISTS Clients
+DROP TABLE IF EXISTS Personnes
+DROP TABLE IF EXISTS Transactions
+DROP TABLE IF EXISTS Parcours
 
 --Création de la table "Clients"
 CREATE TABLE Clients(
 	--Création des colonnes
-	IDClient int IDENTITY (1,1) NOT NULL,
-	IDPersonne int NOT NULL,
+	ClientID int IDENTITY (1,1) NOT NULL,
+	PersonneID int NOT NULL,
 		-- (Oui ou Non?)
-	ClientEstActif nvarchar(3) NULL,
-		--Nombre de numéro dans une carte? 
+	ClientEstActif bit NULL DEFAULT 1,
+		--Nombre de numéro dans une carte? + Parler avec la prof pour NULL ou NOT NULL.
 	NumeroCarteBancaire nvarchar(16) NOT NULL,
 	Email nvarchar(50) NOT NULL,
 		--Je dois tu l'appeler IDParcour ou IDParcourEnCours
-	IDParcoursEnCours int NULL,
+	ParcoursEnCours int NULL,
 	--Création de la clé primaire
 	CONSTRAINT "PK_Clients"
-		PRIMARY KEY CLUSTERED ("IDClient"),
+		PRIMARY KEY CLUSTERED ("ClientID"),
 	--Création des clés étrangère
-	CONSTRAINT "FK_Clients_Clients_1"
-		FOREIGN KEY ("IDParcoursEnCours")
-		REFERENCES "dbo"."Clients" ("IDClient"),
-	CONSTRAINT "FK_Clients_Clients_2"
-		FOREIGN KEY ("IDPersonne")
-		REFERENCES "dbo"."Clients" ("IDClient")
+	CONSTRAINT "FK_Clients_Parcours"
+		FOREIGN KEY ("ParcoursEnCours")
+		REFERENCES "dbo"."Clients" ("ClientID"),
+	CONSTRAINT "FK_Clients_Personnes"
+		FOREIGN KEY ("PersonneID")
+		REFERENCES "dbo"."Clients" ("ClientID")
 	)
 
 --Création de la table "Personnes"
 CREATE TABLE Personnes(
 	--Création des colonnes
-	IDPersonne int IDENTITY (1,1) NOT NULL,
+	PersonneID int IDENTITY (1,1) NOT NULL,
 	Nom nvarchar(20) NOT NULL,
 	Prenom nvarchar(20) NOT NULL,
 	DateNaissance datetime NULL,
 	--Création de la clé primaire
 	CONSTRAINT "PK_Personnes"
-		PRIMARY KEY CLUSTERED ("IDPersonne"),
+		PRIMARY KEY CLUSTERED ("PersonneID"),
 	--Vérifier si la date de naissance est possible
 	CONSTRAINT "CK_DateNaissance"
-		CHECK (DateNaissance < getDate())
+		CHECK (DateNaissance < GETDATE()),
+	CONSTRAINT "CK_Adulte"
+		CHECK (DATEDIFF(year, GETDATE(), DateNaissance) < 18)
 	)
 
 --Création de la table "Transactions"
 CREATE TABLE Transactions(
-	IDtransaction int IDENTITY (1,1) NOT NULL,
-	MontantTransaction nvarchar(5) NOT NULL,
+	transactionID int IDENTITY (1,1) NOT NULL,
+	MontantTransaction decimal NOT NULL,
 	DateTransaction datetime NULL,
 	--Création de la clé primaire
 	CONSTRAINT "PK_Transactions"
-		PRIMARY KEY CLUSTERED ("IDTransaction"),
-	--Vérifier si la date de transaction est possible
-	CONSTRAINT "CK_DateTransaction"
-		CHECK (DateTransaction < getDate())
+		PRIMARY KEY CLUSTERED ("TransactionID")
 	)
 
 --Création de la table "Parcours"
 CREATE TABLE Parcours(
 	--Créations des colonnes
-	IDParcours int IDENTITY (1,1) NOT NULL,
+	ParcoursID int IDENTITY (1,1) NOT NULL,
 	NomDuParcours nvarchar(20) NOT NULL,
-	NiveauDifficulte nvarchar(2) NULL,
+	NiveauDifficulte int NULL,
 		--On le met tu NULL, parce ce qu'on va set les types de parcours?
 	TypeDeParcours nvarchar(10) NOT NULL,
 		--NULL pour le oui ou non
-	EstInstalle nvarchar(3) NULL,
+	EstInstalle bit NULL DEFAULT 1,
 	--Création de la clé primaire
 	CONSTRAINT "PK_Parcours"
-		PRIMARY KEY CLUSTERED ("IDParcours")
+		PRIMARY KEY CLUSTERED ("ParcoursID")
 	)
