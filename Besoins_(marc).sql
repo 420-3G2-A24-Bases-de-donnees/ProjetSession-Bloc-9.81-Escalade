a95cce7ce3857982e7acbfd38118860d445731d3
Marc-Antoine Perron <157721626+LePerron@users.noreply.github.com>
2024-11-21 09:45:43 -0500

    Add files via upload

---
+++ Besoins_(marc).sql
@@ -0,0 +1,15 @@
+-- Premier besoin
+/*Les employées peuvent installer/désinstaller des parcours*/
+
+--Deuxième besoin
+/*Le gérant peut obtenir les profits du centre (VUE)*/ 
+
+-- Troisième besoin
+/*Les ressources humaines peuvent créer des payes*/
+
+-- Quatrième besoin
+/*Les employees peuvent obtenir les parcours actifs. (VUE)*/
+
+-- Cinquième besoin
+/*Le DB admin peut faire quelque chose
+
