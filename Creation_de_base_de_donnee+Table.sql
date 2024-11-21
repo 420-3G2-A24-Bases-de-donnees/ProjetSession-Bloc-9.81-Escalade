f0a4d50da91506a39bec425fb9dae220ff7527f0
benjaminLemoyne <157721721+benjaminLemoyne@users.noreply.github.com>
2024-11-21 10:33:59 -0500

    Pour que ça plus beau

---
+++ Creation_de_base_de_donnee+Table.sql
@@ -0,0 +1,166 @@
+--Création de la base de donnée "Bloc_9.81"
+GO
+CREATE DATABASE [Bloc_9.81]
+COLLATE French_CI_AS
+;
+GO
+
+--2
+USE [Bloc_9.81]
+GO
+
+--3
+DROP TABLE IF EXISTS Personnes
+
+--Création de la table "Personnes"
+CREATE TABLE Personnes(
+	--Création des colonnes
+	PersonneID int IDENTITY (1,1) NOT NULL,
+	Nom nvarchar(20) NOT NULL,
+	Prenom nvarchar(20) NOT NULL,
+	DateNaissance datetime NULL,
+	--Création de la clé primaire
+	CONSTRAINT "PK_Personnes"
+		PRIMARY KEY CLUSTERED ("PersonneID"),
+	--Vérifier si la date de naissance est possible
+	CONSTRAINT "CK_DateNaissance"
+		CHECK (DateNaissance < GETDATE()),
+	CONSTRAINT "CK_Adulte"
+		CHECK (DATEDIFF(year, GETDATE(), DateNaissance) < 18)
+)
+
+--4
+DROP TABLE IF EXISTS Employees
+
+CREATE TABLE Employees(
+	EmployeeID int IDENTITY(1,1) NOT NULL,
+	PersonneID int NOT NULL,
+	DateEngagement datetime NULL,
+	TauxHoraire decimal NULL,
+	NumeroCompteBancaire nvarchar(20) NULL,
+
+	CONSTRAINT "PK_Employees"
+		PRIMARY KEY CLUSTERED ("EmployeeID"),
+
+	CONSTRAINT "FK_Employees_Personnes"
+		FOREIGN KEY ("PersonneID")
+		REFERENCES "dbo"."Personnes" ("PersonneID"),
+
+	CONSTRAINT "CK_DateEngagement"
+		CHECK (DateEngagement BETWEEN '07-10-2020' AND GETDATE()),
+
+	CONSTRAINT "CK_NumeroCompteBancaire"  
+		CHECK (NumeroCompteBancaire LIKE '[0-9]{5}-[0-9]{2}-[0-9]{5}')
+)
+
+--5
+
+DROP TABLE IF EXISTS Parcours
+
+--Création de la table "Parcours"
+CREATE TABLE Parcours(
+	--Créations des colonnes
+	ParcoursID int IDENTITY (1,1) NOT NULL,
+	NomDuParcours nvarchar(20) NOT NULL,
+	NiveauDifficulte int NULL,
+		--On le met tu NULL, parce ce qu'on va set les types de parcours?
+	TypeDeParcours nvarchar(10) NOT NULL,
+		--NULL pour le oui ou non
+	EstInstalle bit NULL DEFAULT 1,
+	--Création de la clé primaire
+	CONSTRAINT "PK_Parcours"
+		PRIMARY KEY CLUSTERED ("ParcoursID")
+	--Validation pour que le niveau de difficulte ne soit pas négatiff ???
+)
+
+--6
+
+DROP TABLE IF EXISTS Transactions
+
+DROP TABLE IF EXISTS Clients
+
+--Création de la table "Clients"
+CREATE TABLE Clients(
+	--Création des colonnes
+	ClientID int IDENTITY (1,1) NOT NULL,
+	PersonneID int NOT NULL,
+		-- (Oui ou Non?)
+	ClientEstActif bit NULL DEFAULT 1,
+		--Nombre de numéro dans une carte? + Parler avec la prof pour NULL ou NOT NULL. + DOIT ÊTRE CHIFFRER
+	NumeroCarteBancaire nvarchar(16) NOT NULL,
+	Email nvarchar(50) NOT NULL,
+		--Je dois tu l'appeler IDParcour ou IDParcourEnCours
+	ParcoursEnCours int NULL,
+	--Création de la clé primaire
+	CONSTRAINT "PK_Clients"
+		PRIMARY KEY CLUSTERED ("ClientID"),
+	--Création des clés étrangère
+	CONSTRAINT "FK_Clients_Parcours"
+		FOREIGN KEY ("ParcoursEnCours")
+		REFERENCES "dbo"."Clients" ("ClientID"),
+	CONSTRAINT "FK_Clients_Personnes"
+		FOREIGN KEY ("PersonneID")
+		REFERENCES "dbo"."Clients" ("ClientID"),
+	--Vérification pour que l'addresse courriel soit valide
+	CONSTRAINT "CK_Email"
+		CHECK (Email LIKE '%@%')
+)
+
+--7
+
+DROP TABLE IF EXISTS Transactions
+
+--Création de la table "Transactions"
+CREATE TABLE Transactions(
+	transactionID int IDENTITY (1,1) NOT NULL,
+	-- DOIT ÊTRE CHIFFRER
+	MontantTransaction decimal NOT NULL,
+	DateTransaction datetime NULL,
+	--Création de la clé primaire
+	CONSTRAINT "PK_Transactions"
+		PRIMARY KEY CLUSTERED ("TransactionID")
+)
+
+--8
+
+DROP TABLE IF EXISTS Payes
+
+CREATE TABLE Payes(
+	PayeID int IDENTITY(1,1) NOT NULL,
+	TransactionID int NOT NULL,
+	EmployeeID int NOT NULL,
+
+	CONSTRAINT "PK_Payes"
+		PRIMARY KEY CLUSTERED ("PayeID"),
+
+	CONSTRAINT "FK_Payes_Transactions"
+		FOREIGN KEY ("TransactionID")
+		REFERENCES "dbo"."Transactions" ("TransactionID"),
+
+	CONSTRAINT "FK_Payes_Employees"
+		FOREIGN KEY ("EmployeeID")
+		REFERENCES "dbo"."Employees" ("EmployeeID"),
+)
+
+--9
+
+DROP TABLE IF EXISTS Visites
+
+CREATE TABLE Visites(
+	VisiteID int IDENTITY(1,1) NOT NULL,
+	ClientID int NOT NULL,
+	HeureEntree datetime NULL, -- À Vérifier
+	HeureSortie datetime NULL, -- À Vérifier
+	TransactionID int NULL, -- À Vérifier
+
+	CONSTRAINT "PK_Visites"
+		PRIMARY KEY CLUSTERED ("VisiteID"),
+
+	CONSTRAINT "FK_Visites_Clients"
+		FOREIGN KEY ("ClientID")
+		REFERENCES "dbo"."Clients" ("ClientID"),
+
+	CONSTRAINT "FK_Visites_Transactions"
+		FOREIGN KEY ("TransactionID")
+		REFERENCES "dbo"."Transactions" ("TransactionID"),
+)
