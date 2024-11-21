e9ca0417f8033c56e815182c376d7d90e7013776
Marc-Antoine Perron <157721626+LePerron@users.noreply.github.com>
2024-11-21 09:19:01 -0500

    Add files via upload

---
+++ SEEDING_Questions (ChatGPT).sql
@@ -0,0 +1,24 @@
+-- DEMANDE CHATGPT (généré des données)
+
+---- Personnes : Donne moi 100 script qui seed dans cette base de données, donne moi 100 données de 'Personnes' pour cette base de donnée: 
+	-- PersonneID int IDENTITY (1,1) NOT NULL,
+	-- Nom nvarchar(20) NOT NULL,
+	-- Prenom nvarchar(20) NOT NULL,
+	-- DateNaissance datetime NULL)
+
+---- Employees : 
+	-- EmployeeID int IDENTITY(1,1) NOT NULL,
+	-- PersonneID int NOT NULL,
+	-- DateEngagement datetime NULL,
+	-- TauxHoraire decimal NULL,
+	-- NumeroCompteBancaire nvarchar(20) NULL,
+
+
+--Donne moi 100 script qui seed dans cette base de données, donne moi 100 données de 'Personnes' pour cette base de donnée:  (le PersonneID doit commencer a 100)
+
+--le format de NumeroCompteBancaire : xxxxx-xx-xxxxx (ou x = n'importe quel chiffre)
+
+--il peu y avoir quelques données vides, à condition que la column le permet
+
+
+--
