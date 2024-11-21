bd4b1c01759368f6b5e3cd6064dc05faaefd4f00
benjaminLemoyne <157721721+benjaminLemoyne@users.noreply.github.com>
2024-11-21 09:29:39 -0500

    Les besoin de (BEN)
    Mes besoins....

---
+++ Besoins_(BEN).sql
@@ -0,0 +1,17 @@
+--Premier besoin
+/* Les employees peuvent ajouter un parcours dans la table "Parcours"*/
+
+
+--Deuxième besoin
+/* Le gérant peut ajouter un employee dans la table "Employees"*/
+
+--Troisième besoin
+/*Les employees peuvent enregistrer un nouveau client dans la table  "Clients"*/
+
+--Quatrième besoin
+/*Les employees peuvent enregistrer une visite d'un client dans la table "Visites"*/
+
+--Cinquième besoin
+/*Le gérant peut changer le taux d'horaire d'un employee*/
+
+
