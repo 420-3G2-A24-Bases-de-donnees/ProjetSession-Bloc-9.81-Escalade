812b0025a943983ed3f7ad513a360499d45897e0
benjaminLemoyne <157721721+benjaminLemoyne@users.noreply.github.com>
2024-11-25 13:37:16 -0600

    je fais de mon mieux okay

--- Besoins_(BEN).sql
+++ Besoins_(BEN).sql
@@ -1,9 +1,18 @@
+USE [Bloc_9.81]
+GO
 --Premier besoin
-/* Les employees peuvent ajouter un parcours dans la table "Parcours"*/
+/* Les employees peuvent afficher le nombre de Clients actif "ClientsEstActif" = 1 ou "ClientsEstActif" = 0*/
+SELECT Prenom, Nom, DateNaissance, Email FROM Clients INNER JOIN Personnes
+ON Clients.PersonneID = Personnes.PersonneID
+WHERE ClientEstActif = 1
 
+SELECT Prenom, Nom, DateNaissance, Email FROM Clients INNER JOIN Personnes
+ON Clients.PersonneID = Personnes.PersonneID
+WHERE ClientEstActif = 0
 
 --Deuxième besoin
 /* Le gérant peut ajouter un employee dans la table "Employees"*/
+
 
 --Troisième besoin
 /*Les employees peuvent enregistrer un nouveau client dans la table  "Clients"*/
