00742b75581a148772f13ee245d4fe3e594a636b
Marc-Antoine Perron <157721626+LePerron@users.noreply.github.com>
2024-11-27 13:46:03 -0500

    Add files via upload

--- Besoins_(marc).sql
+++ Besoins_(marc).sql
@@ -2,14 +2,18 @@
 /*Les employées peuvent installer/désinstaller des parcours*/
 
 
---Deuxième besoin (Fonction ET Vue)
+--Deuxième besoin (Fonction ET Vue) FAIT
 /*Le gérant peut obtenir les profits du centre*/ 
 
-CREATE OR ALTER FUNCTION fnCalculerRevenuBrutMensuel()
-RETURNS @RevenuBrutMensuels TABLE (Mois nvarchar(20), RevenuBrut decimal)
+CREATE OR ALTER FUNCTION fnCalculerRevenuBrutMensuelsDeAnnee(@Annee int=null)
+RETURNS @RevenuBrutMensuelsDeAnnee TABLE (Mois nvarchar(20), RevenuBrut decimal)
 AS
 BEGIN;
-	INSERT INTO @RevenuBrutMensuels (Mois, RevenuBrut)
+	IF @Annee = null
+	BEGIN
+		SET @Annee = YEAR(GETDATE())
+	END
+	INSERT INTO @RevenuBrutMensuelsDeAnnee (Mois, RevenuBrut)
     SELECT 
         DATENAME(MONTH, DATEADD(MONTH, MONTH(DateTransaction) - 1, 0)) AS Mois,
         SUM(MontantTransaction) AS RevenuBrut
@@ -17,37 +21,45 @@ BEGIN;
         Transactions
     WHERE 
         TransactionID NOT IN (SELECT TransactionID FROM Payes)
+		AND YEAR(DateTransaction) = @Annee
     GROUP BY 
         MONTH(DateTransaction)
     
     DECLARE @mois int = 1;
     WHILE @mois <= 12
     BEGIN
-        IF NOT EXISTS (SELECT 1 FROM @RevenuBrutMensuels WHERE Mois = DATENAME(MONTH, DATEADD(MONTH, @mois - 1, 0)))
+        IF NOT EXISTS (SELECT 1 FROM @RevenuBrutMensuelsDeAnnee WHERE Mois = DATENAME(MONTH, DATEADD(MONTH, @mois - 1, 0)))
         BEGIN
-            INSERT INTO @RevenuBrutMensuels (Mois, RevenuBrut) VALUES (DATENAME(MONTH, DATEADD(MONTH, @mois - 1, 0)), 0);
+            INSERT INTO @RevenuBrutMensuelsDeAnnee (Mois, RevenuBrut) VALUES (DATENAME(MONTH, DATEADD(MONTH, @mois - 1, 0)), 0);
         END
         SET @mois = @mois + 1;
     END
     RETURN
 END;
 
+
+
 GO
-CREATE OR ALTER FUNCTION fnCalculerPayesMensuel()
+CREATE OR ALTER FUNCTION fnCalculerPayesMensuelsDeAnnee(@Annee int = null)
 RETURNS @PayesMensuel TABLE (Mois nvarchar(20), Payes decimal)
 AS
 BEGIN;
+	IF @Annee = null
+	BEGIN 
+		SET @Annee = YEAR(GETDATE())
+	END
     INSERT INTO @PayesMensuel (Mois, Payes)
     SELECT 
         DATENAME(MONTH, DATEADD(MONTH, MONTH(DateTransaction) - 1, 0)) AS Mois,
         SUM(MontantTransaction) AS Payes
-    FROM 
+        FROM 
         Payes
-	INNER JOIN 
+    INNER JOIN 
         Transactions ON Payes.TransactionID = Transactions.TransactionID
+    WHERE 
+        YEAR(DateTransaction) = @Annee
     GROUP BY 
-        MONTH(DateTransaction)
-    
+        MONTH(DateTransaction);
     DECLARE @mois int = 1;
     WHILE @mois <= 12
     BEGIN
@@ -62,27 +74,49 @@ END;
 
 
 GO
-CREATE OR ALTER VIEW ProfitsDuCentreParMois
+-- VIEW qui permet d'Obtenir les profits du centre pour chaque mois 
+CREATE OR ALTER VIEW ProfitsDuCentreParMoisDerniereAnnee
 AS
 	SELECT 
-		r.Mois,
-		FORMAT(r.RevenuBrut - p.Payes, 'C', 'fr-CA') AS Profit
+		dbo.Capitaliser(r.Mois) AS Mois,
+		CONCAT(r.RevenuBrut - p.Payes, ' $') AS Profit
 	FROM 
-		fnCalculerRevenuBrutMensuel() r
+		fnCalculerRevenuBrutMensuelsDeAnnee(2024) r
 	LEFT JOIN 
-		fnCalculerPayesMensuel() p ON r.Mois = p.Mois;
+		fnCalculerPayesMensuelsDeAnnee(2024) p ON r.Mois = p.Mois;
 GO
-
-
 SELECT * FROM ProfitsDuCentreParMois
 
+SELECT * FROM VISITEs INNER JOIN Transactions ON Visites.TransactionID = transactions.transactionID
+GO
+CREATE OR ALTER FUNCTION Capitaliser(@chaine nvarchar(256))
+RETURNS nvarchar(256)
+AS
+BEGIN;
+    IF @chaine IS NULL OR @chaine = ''
+        RETURN @chaine;
+    RETURN CONCAT(UPPER(LEFT(@chaine, 1)), LOWER(SUBSTRING(@chaine, 2, LEN(@chaine) - 1)));
+END;
+
+SELECT * FROM fnCalculerRevenuBrutMensuelsDeAnnee(2024)
+
+
+		-- DÉCLENCHEUR
+		-- à l'ajout d'un parcours mettre ACTIF
 
 -- Troisième besoin
 /*Les ressources humaines peuvent créer des payes*/
 
 -- Quatrième besoin
 /*Les employees peuvent obtenir les parcours actifs. (VUE)*/
+--aussi avec colonne actif depuis quand
+
 
 -- Cinquième besoin
 /*Le DB admin peut faire quelque chose
 
+
+
+-- DÉCLENCHEUR quand un parcours n'est pas installé, les client associé sont remis a aucun
+
+obtenir le nombre de parcours par difficulté
