bb543dfd1181c0b0d3c8cc646d9ddc008ce51042
Marc-Antoine Perron <157721626+LePerron@users.noreply.github.com>
2024-11-28 13:28:02 -0500

    Update 06 - Utilisateur Lemoine Héléne (Employée).sql

--- A mettre/06 - Utilisateur Lemoine Héléne (Employée).sql
+++ A mettre/06 - Utilisateur Lemoine Héléne (Employée).sql
@@ -1,4 +1,120 @@
 USE [Bloc_9.81]
-CREATE USER [Gamier H�l�ne] FOR LOGIN [Bloc9.81];
 
-DROP USER [Gamier H�l�ne]
+DROP USER [Gamier Héléne]
+
+-- Employee
+CREATE USER [Gamier Héléne] FOR LOGIN [Bloc9.81];
+
+
+use [Bloc_9.81]
+GO
+DENY INSERT ON [dbo].[Payes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY ALTER ON [dbo].[Payes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Payes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Payes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY INSERT ON [dbo].[Employees] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY ALTER ON [dbo].[Employees] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Employees] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Employees] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+GRANT INSERT ON [dbo].[Parcours] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+GRANT ALTER ON [dbo].[Parcours] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Parcours] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Parcours] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+GRANT INSERT ON [dbo].[Visites] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+GRANT ALTER ON [dbo].[Visites] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Visites] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Visites] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+GRANT INSERT ON [dbo].[Clients] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+GRANT ALTER ON [dbo].[Clients] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Clients] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Clients] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY INSERT ON [dbo].[Transactions] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY ALTER ON [dbo].[Transactions] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Transactions] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Transactions] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY INSERT ON [dbo].[Personnes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY ALTER ON [dbo].[Personnes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY SELECT ON [dbo].[Personnes] TO [Gamier Héléne]
+GO
+use [Bloc_9.81]
+GO
+DENY DELETE ON [dbo].[Personnes] TO [Gamier Héléne]
+GO
